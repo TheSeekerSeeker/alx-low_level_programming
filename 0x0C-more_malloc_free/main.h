8bde99c5152aa801b9a40000a7340ef77364023a
TheSeekerSeeker <odondiezra5@gmail.com>
2022-04-07 16:58:25 +0300

    more alloc,free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _main_H
+#define _main_H
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+#endif
